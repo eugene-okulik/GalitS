de05693108a1d1ddcf7969085b6580b8456393fa
Galit-QA <galitshmr@gmail.com>
2026-02-04 00:41:59 +0200

    Fixed 4

--- homework/galit_shemer/Lesson_4/task_dictionary.py
+++ homework/galit_shemer/Lesson_4/task_dictionary.py
@@ -1,4 +1,4 @@
-def main():
+def create_my_dict():
     my_dict = {
         'tuple': (1, 2, 3, 4, 5),
         'list': [10, 20, 30, 40, 50],
@@ -12,6 +12,10 @@ def main():
         'set': {100, 200, 300, 400, 500}
     }
 
+    return my_dict
+
+
+def modify_my_dict(my_dict):
     print(my_dict['tuple'][-1])
 
     my_dict['list'].append(60)
@@ -23,10 +27,18 @@ def main():
     my_dict['set'].add(600)
     my_dict['set'].remove(100)
 
+
+def print_my_dict(my_dict):
     print("tuple:", my_dict['tuple'])
     print("list:", my_dict['list'])
     print("dict:", my_dict['dict'])
     print("set:", my_dict['set'])
+
+
+def main():
+    my_dict = create_my_dict()
+    modify_my_dict(my_dict)
+    print_my_dict(my_dict)
 
 
 if __name__ == "__main__":

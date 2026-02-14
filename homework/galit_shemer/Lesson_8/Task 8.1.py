e8123889de023c48b2ab1c5b2632a7f9807f695b
Galit-QA <galitshmr@gmail.com>
2026-02-14 23:59:06 +0200

    Refactor file names to correct format

--- homework/galit_shemer/Lesson_8/Task 8.1.py
+++ homework/galit_shemer/Lesson_8/Task 8.1.py
@@ -1,34 +1,16 @@
 import random
-import sys
-
-sys.set_int_max_str_digits(0)
-
-salary = int(input("Введите зарплату: "))
-bonus = random.choice([True, False])
-
-if bonus:
-    salary += random.randint(1, salary)
-
-print(f"{salary}, {bonus} - '${salary}'")
 
 
-def fibonacci_generator():
-    a, b = 0, 1
-    while True:
-        yield a
-        a, b = b, a + b
+def main():
+    base_salary = int(input("Введите зарплату: "))
+    bonus = random.choice([True, False])
+
+    final_salary = base_salary
+    if bonus:
+        final_salary += random.randint(1, base_salary)
+
+    print(f"{base_salary}, {bonus} - '${final_salary}'")
 
 
-targets = {5, 200, 1000, 100000}
-results = {}
-
-for i, x in enumerate(fibonacci_generator(), 1):
-    if i in targets:
-        results[i] = x
-        if len(results) == len(targets):
-            break
-
-print(results[5])
-print(results[200])
-print(results[1000])
-print(results[100000])
+if __name__ == "__main__":
+    main()

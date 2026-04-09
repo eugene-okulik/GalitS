30b646ab7c59b3092e352631a10ac213c9b16f4c
Galit-QA <galitshmr@gmail.com>
2026-04-09 23:45:11 +0300

    fix flake8 lesson 7 and 10 formatting

--- homework/galit_shemer/Lesson_10/task10.1.py
+++ homework/galit_shemer/Lesson_10/task10.1.py
@@ -13,7 +13,9 @@ def finish_me(func):
 
 def repeat_me(func=None, *, count=1):
     if func is None:
+
         def decorator(inner_func):
+
             @wraps(inner_func)
             def wrapper(*args, **kwargs):
                 for _ in range(count):
@@ -30,74 +32,3 @@ def repeat_me(func=None, *, count=1):
             func(*args, **kwargs)
 
     return wrapper
-
-
-def operation_control(func):
-    @wraps(func)
-    def wrapper(first, second):
-        if first == second:
-            return func(first, second, "+")
-        if first < 0 or second < 0:
-            return func(first, second, "*")
-        if first > second:
-            return func(first, second, "-")
-        return func(first, second, "/")
-
-    return wrapper
-
-
-@operation_control
-def calc(first, second, operation):
-    if operation == "+":
-        return first + second
-    if operation == "-":
-        return first - second
-    if operation == "*":
-        return first * second
-    if operation == "/":
-        return first / second
-
-
-PRICE_LIST = """тетрадь 50р
-книга 200р
-ручка 100р
-карандаш 70р
-альбом 120р
-пенал 300р
-рюкзак 500р"""
-
-
-price_dict = {
-    item.split()[0]: int(item.split()[1][:-1])
-    for item in PRICE_LIST.split("\n")
-}
-
-
-@finish_me
-def example(text):
-    print(text)
-
-
-@repeat_me
-def example_repeat(text):
-    print(text)
-
-
-@repeat_me(count=2)
-def example_repeat2(text):
-    print(text)
-
-
-if __name__ == "__main__":
-    example("print me")
-
-    example_repeat("print me", count=2)
-
-    example_repeat2("print me")
-
-    print(calc(5, 5))
-    print(calc(10, 5))
-    print(calc(5, 10))
-    print(calc(-2, 3))
-
-    print(price_dict)

e989c16844c799ed9bdb87d138196ed227c5a079
Galit-QA <galitshmr@gmail.com>
2026-04-09 22:58:57 +0300

    rewrite lesson 7 task 1 clean solution

--- homework/galit_shemer/Lesson_7/task7.1.py
+++ homework/galit_shemer/Lesson_7/task7.1.py
@@ -1,9 +1,11 @@
 secret_number = 7
 
-while True:
-    guess = int(input("Угадайте число: "))
-    if guess == secret_number:
-        print("Поздравляю! Вы угадали!")
-        break
-    else:
+guess = None
+
+while guess != secret_number:
+    guess = int(input("Угадай число: "))
+
+    if guess != secret_number:
         print("Попробуйте снова")
+
+print("Поздравляю! Вы угадали!")

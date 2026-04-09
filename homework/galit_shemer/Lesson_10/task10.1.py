2423586fb910367bc1d85c5a7d44848e509f1c78
Galit-QA <galitshmr@gmail.com>
2026-04-09 22:28:00 +0300

    rewrite lesson 10 clean solution

--- homework/galit_shemer/Lesson_10/task10.1.py
+++ homework/galit_shemer/Lesson_10/task10.1.py
@@ -1,11 +1,13 @@
 from functools import wraps
 
+
 def finish_me(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         result = func(*args, **kwargs)
         print("finished")
         return result
+
     return wrapper
 
 
@@ -16,13 +18,17 @@ def repeat_me(func=None, *, count=1):
             def wrapper(*args, **kwargs):
                 for _ in range(count):
                     inner_func(*args, **kwargs)
+
             return wrapper
+
         return decorator
+
     @wraps(func)
     def wrapper(*args, **kwargs):
-        times = kwargs.pop("count", 1)
+        times = kwargs.pop("count", count)
         for _ in range(times):
             func(*args, **kwargs)
+
     return wrapper
 
 
@@ -30,19 +36,45 @@ def operation_control(func):
     @wraps(func)
     def wrapper(first, second):
         if first == second:
-            op = "+"
-        elif first < 0 or second < 0:
-            op = "*"
-        elif first > second:
-            op = "-"
-        else:
-            op = "/"
-        return func(first, second, op)
+            return func(first, second, "+")
+        if first < 0 or second < 0:
+            return func(first, second, "*")
+        if first > second:
+            return func(first, second, "-")
+        return func(first, second, "/")
+
     return wrapper
 
 
+@operation_control
+def calc(first, second, operation):
+    if operation == "+":
+        return first + second
+    if operation == "-":
+        return first - second
+    if operation == "*":
+        return first * second
+    if operation == "/":
+        return first / second
+
+
+PRICE_LIST = """тетрадь 50р
+книга 200р
+ручка 100р
+карандаш 70р
+альбом 120р
+пенал 300р
+рюкзак 500р"""
+
+
+price_dict = {
+    item.split()[0]: int(item.split()[1][:-1])
+    for item in PRICE_LIST.split("\n")
+}
+
+
 @finish_me
-def example_finish(text):
+def example(text):
     print(text)
 
 
@@ -52,41 +84,20 @@ def example_repeat(text):
 
 
 @repeat_me(count=2)
-def example_repeat_param(text):
+def example_repeat2(text):
     print(text)
 
 
-@operation_control
-def calc(first, second, operation):
-    if operation == "+":
-        return first + second
-    elif operation == "-":
-        return first - second
-    elif operation == "*":
-        return first * second
-    elif operation == "/":
-        return first / second
+if __name__ == "__main__":
+    example("print me")
 
+    example_repeat("print me", count=2)
 
-example_finish("print me")
-example_repeat("print me", count=2)
-example_repeat_param("print me")
+    example_repeat2("print me")
 
-a = float(input())
-b = float(input())
-print(calc(a, b))
+    print(calc(5, 5))
+    print(calc(10, 5))
+    print(calc(5, 10))
+    print(calc(-2, 3))
 
-PRICE_LIST = '''тетрадь 50р
-книга 200р
-ручка 100р
-карандаш 70р
-альбом 120р
-пенал 300р
-рюкзак 500р'''
-
-price_dict = {
-    item.split()[0]: int(item.split()[1].replace("р", ""))
-    for item in PRICE_LIST.splitlines()
-}
-
-print(price_dict)
+    print(price_dict)

0e9a8bf27e192186e0ee44399118590645013166
Galit-QA <galitshmr@gmail.com>
2026-04-09 22:56:47 +0300

    rewrite lesson 7 task 3 clean solution

--- homework/galit_shemer/Lesson_7/task7.3.py
+++ homework/galit_shemer/Lesson_7/task7.3.py
@@ -1,14 +1,15 @@
-def process_result_line(line):
-    parts = line.split()
-    number = int(parts[-1])
+def extract_and_add_10(text):
+    number = int(text.split(":")[-1].strip())
     return number + 10
-lines = [
+
+
+results = [
     "результат операции: 42",
     "результат операции: 54",
     "результат работы программы: 209",
-    "результат: 2"
+    "результат: 2",
 ]
 
-for line in lines:
-    result = process_result_line(line)
-    print(result)
+
+for item in results:
+    print(extract_and_add_10(item))
